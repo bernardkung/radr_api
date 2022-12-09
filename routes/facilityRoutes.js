1ca287b262edf1f5634562464cdb6ffce293fb80
bernard kung <speak.freely@gmail.com>
2022-12-09 15:59:50 -0500

    reduced facility routes

--- routes/facilityRoutes.js
+++ routes/facilityRoutes.js
@@ -8,45 +8,79 @@ const { models }    = require('../sequelize');
 
 // INDEX
 router.get("/", async (req, res)=>{
-  const facilities = await models.facility.findAll();
-  res.render('facilities/index', {facilities});
-});
+  try {
+    const facilities = await models.facility.findAll()
+    res.status(200).json({ facilities })
+  } catch (err) {
+    res.status(400).send(err)
+  }
+})
 
 // NEW
 router.get("/new", (req, res)=>{
-  res.render('facilities/create');
+  try {
+    res.status(204).end()
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 // CREATE
 router.post("/new", async (req, res)=>{
-  const facility = await models.facility.create(req.body.facility);
-  res.redirect(`/facilities/${facility.id}`);
+  try {
+    const facility = await models.facility.create(req.body.facility)
+    res.status(201).end()
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 // SHOW
 router.get("/:id", async (req, res)=>{
-  const facility = await models.facility.findByPk(req.params.id);
-  res.render('facilities/show', {facility});
+  try {
+    const facility = await models.facility.findByPk(req.params.id)
+    if (!facility) {
+      res.status(404).end()
+    } else {
+      res.status(200).json({facility})
+    }
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 // EDIT
 router.get("/:id/edit", async (req, res)=>{
-  const facility = await models.facility.findByPk(req.params.id);
-  res.render('facilities/update', {facility});
+  try {
+    const facility = await models.facility.findByPk(req.params.id)
+    res.status(200).json({facility})
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 // UPDATE
 router.put("/:id", async (req, res)=>{
-  const facility = await models.facility.findByPk(req.params.id);
-  await facility.update(req.body.facility)
-  res.redirect(`/facilities/${facility.id}`);
+  try {
+    const facility = await models.facility.findByPk(req.params.id);
+    await facility.update(req.body.facility)
+    res.status(201).end()
+  } catch (err) {
+    res.status(400).send(err)
+  }
+
 })
 
 // DESTROY
 router.delete("/:id", async (req, res)=>{
-  const facility = await models.facility.findByPk(req.params.id);
-  await facility.destroy();
-  res.redirect('/facilities');
+  //
+  try {
+    const facility = await models.facility.findByPk(req.params.id)
+    await facility.destroy()
+    res.redirect('/facilities')
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 
