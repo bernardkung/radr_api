5d342600876602c067e49c9d0fa98ff8eafbb282
bernard kung <speak.freely@gmail.com>
2023-01-12 16:38:57 -0500

    updated auditor routes

--- routes/auditorRoutes.js
+++ routes/auditorRoutes.js
@@ -7,45 +7,95 @@ const { models }    = require('../sequelize')
 
 // INDEX
 router.get("/", async (req, res)=>{
-    const auditors = await models.auditor.findAll();
-    res.render('auditors/index', {auditors});
-  });
+  try {
+      const auditors = await models.auditor.findAll()
+      res.status(200).json({ auditors })
+    } catch (err) {
+      res.status(400).send(err)
+    }
+})
 
 // NEW
 router.get("/new", (req, res)=>{
-    res.render('auditors/create')
+  try {
+    res.status(204).end()
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 // CREATE
 router.post('/new', async (req, res)=>{
+  try {
     const auditor = await models.auditor.create(req.body.auditor)
-    res.redirect(`/auditors/`)
+    res.status(201).end()
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 // SHOW
 router.get('/:id', async (req, res)=>{
+  try {
     const auditor = await models.auditor.findByPk(req.params.id)
-    res.render('auditors/show', {auditor})
+    if (!auditor) {
+      // If no auditor found
+      res.status(404).send("no Auditor found under given ID")
+    } else {
+      res.status(200).json({auditor})
+    }
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 // EDIT
 router.get('/:id/edit', async (req, res)=>{
+  try {
     const auditor = await models.auditor.findByPk(req.params.id)
-    res.render('auditors/update', {auditor})
+    if (!auditor) {
+      // If no auditor found
+      res.sendStatus(404).send("no Auditor found under given ID")
+    } else {
+      res.status(200).json({ auditor })
+    }
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 // UPDATE
 router.put(':/id', async (req, res)=>{
-    const auditor = await models.auditor.findByPk(req.params.id)
-    await auditor.update(req.body.auditor)
-    res.redirect(`/auditors/${auditor.id}`)
+  try {
+    const auditor = await models.auditor.findByPk(req.params.id);
+    if (!auditor) {
+      // If no auditor found
+      res.sendStatus(404).send("no Auditor found under given ID")
+    } else {
+      await auditor.update(req.body.auditor)
+      res.status(201).end()
+    }
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 // DESTROY
 router.delete('/:id', async (req, res)=>{
-    const auditor = await models.auditor.findByPk(req.params.id)
-    await auditor.destroy()
-    res.redirect('/auditors')
+  //
+  try {
+    const auditor = await models.auditor.findByPk(req.params.id)    
+    if (!auditor) {
+      // If no facility found
+      res.status(404).send("no Auditor found under given ID")
+    } else {
+      // Destroy
+      await auditor.destroy()
+      res.status(201).end()
+    }
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 
