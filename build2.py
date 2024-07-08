f38056bbe47c2bbbea81c1534eb5eaa98ee05761
bernard kung <speak.freely@gmail.com>
2024-07-08 16:37:27 -0400

    swapping to ORM

--- build2.py
+++ build2.py
@@ -1,92 +1,134 @@
-from sqlalchemy import MetaData, Table, Column, Integer, Float, String, Date, Boolean, ForeignKey, create_engine, insert
+# from sqlalchemy import MetaData, Table, Column, Integer, Float, String, Date, Boolean, ForeignKey, create_engine, insert
+from sqlalchemy import create_engine, DateTime
+import generator
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+import datetime
 
 # CONNECTION
 engine = create_engine("sqlite+pysqlite:///radr.db", echo=True)
 
 
 # DEFINING TABLES
-metadata_obj = MetaData()
+class Base(DeclarativeBase):
+	pass
 
-facilities_tables = Table(
-    "facilities",
-    metadata_obj,
-    Column("global_id", Integer, primary_key=True),
-    Column("dl_id", Integer),
-    Column("dl_name", String),
-    Column("mac", String),
-    Column("npi", Integer),
-    Column("revenue_center", String),
-)
+class Facility(Base):
+	__tablename__ = "facility"
 
-auditors_table = Table(
-    "auditors",
-    metadata_obj,
-    Column("auditor_id", Integer, primary_key=True),
-    Column("auditor_name", String),
-)
+	global_id: Mapped[int] = mapped_column(primary_key=True)
+	dl_id: Mapped[int]
+	dl_name: Mapped[str]
+	mac: Mapped[str]
+	npi: Mapped[int]
+	revenue_center: Mapped[str]
+	created_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	updated_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
 
-patients_table = Table(
-    "patients",
-    metadata_obj,
-    Column("mrn", Integer, primary_key=True),
-    Column("first_name", String),
-    Column("last_name", String),
-)
+	def __repr__(self) -> str:
+			return f"Facility(global_id={self.global_id!r}, dl_id={self.dl_id!r}, dl_name={self.dl_name!r}), mac={self.mac!r}), npi={self.npi!r}), revenue_center={self.revenue_center!r})"
 
-adrs_table = Table(
-    "adrs",
-    metadata_obj,
-    Column("adr_id", Integer, primary_key=True),
-    Column("global_id", ForeignKey("facilities.global_id"), nullable=False),
-    Column("mrn", ForeignKey("patients.mrn"), nullable=False),
-    Column("from_date", Date),
-    Column("to_date", Date),
-    Column("expected_reimbursement", Float),
-    Column("active", Boolean, nullable=False),
-)
 
-stages_table = Table(
-    "stages",
-    metadata_obj,
-    Column("stage_id", Integer, primary_key=True),
-    Column("adr_id", ForeignKey("adrs.adr_id"), nullable=False),
-    Column("stage", String, nullable=False),
-    Column("notification_date", Date),
-    Column("due_date", Date),
-)
+class Auditor(Base):
+	__tablename__ = "auditor"
 
-submissions_table = Table(
-    "submissions",
-    metadata_obj,
-    Column("submission_id", Integer, primary_key=True),
-    Column("stage_id", ForeignKey("stages.stage_id"), nullable=False),
-    Column("auditor_id", ForeignKey("auditors.auditor_id"), nullable=False),
-    Column("submission_date", Date, nullable=False),
-)
+	auditor_id: Mapped[int] = mapped_column(primary_key=True)
+	auditor_name: Mapped[str]
+	created_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	updated_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
 
-decisions_table = Table(
-    "decisions",
-    metadata_obj,
-    Column("decision_id", Integer, primary_key=True),
-    Column("stage_id", ForeignKey("stages.stage_id"), nullable=False),
-    Column("decision_date", Date, nullable=False),
-    Column("decision", Date, nullable=False),
-)
 
-srns_table = Table(
-    "srns",
-    metadata_obj,
-    Column("srn", String, primary_key=True),
-    Column("adr_id", ForeignKey("adrs.adr_id"), nullable=False),
-)
+class Patient(Base):
+	__tablename__ = "patient"
 
-dcns_table = Table(
-    "dcns",
-    metadata_obj,
-    Column("dcn", String, primary_key=True),
-    Column("adr_id", ForeignKey("adrs.adr_id"), nullable=False),
-)
+	mrn: Mapped[int] = mapped_column(primary_key=True)
+	first_name: Mapped[str]
+	last_name: Mapped[str]
+	created_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	updated_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+
+
+class Adr(Base):
+	__tablename__ = "adr"
+
+	adr_id: Mapped[int] = mapped_column(primary_key=True)
+	global_id: Mapped[Facility] = relationship()
+	mrn: Mapped[Patient] = relationship()
+	from_date: Mapped[datetime.date]
+	to_date: Mapped[datetime.date]
+	expected_reimbursement: Mapped[float]
+	active: Mapped[bool]
+	created_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	updated_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	
+
+class Stage(Base):
+	__tablename__ = "table"
+
+	stage_id: Mapped[int] = mapped_column(primary_key=True)
+	adr_id: Mapped[Adr] = relationship()
+	stage: Mapped[str]
+	notification_date: Mapped[datetime.date]
+	due_date: Mapped[datetime.date]
+	created_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	updated_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	
+
+class Submission(Base):
+	__tablename__ = "submission"
+
+	submission_id: Mapped[int] = mapped_column(primary_key=True)
+	stage_id: Mapped[Stage] = relationship()
+	auditor_id: Mapped[Auditor] = relationship()
+	submission_date: Mapped[datetime.date]
+	created_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	updated_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	
+
+class Decision(Base):
+	__tablename__ = "decision"
+
+	decision_id: Mapped[int] = mapped_column(primary_key=True)
+	stage_id: Mapped[Stage] = relationship()
+	decision_date: Mapped[datetime.date]
+	decision: Mapped[str]
+	created_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	updated_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	
+
+class Srn(Base):
+	__tablename__ = "srn"
+
+	srn: Mapped[str] = mapped_column(primary_key=True)
+	adr_id: Mapped[Stage] = relationship()
+	created_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	updated_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	
+
+class Dcn(Base):
+	__tablename__ = "dcn"
+
+	dcn: Mapped[str] = mapped_column(primary_key=True)
+	adr_id: Mapped[Stage] = relationship()
+	created_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	updated_date: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+	
+
 
 
 # Build Tables
-metadata_obj.create_all(engine)
+Base.metadata.create_all(engine)
+
+
+# Generate Random Data
+data = generator.generate_data(export=False)
+
+# Insert into Tables
